5d5191a2ce430515c687843d4160f1547cb5a03a
Sofia Levin <sofiafontes@me.com>
2020-08-31 13:53:10 -0700

    Adding movies to Nominated section.

--- src/components/NominatedList.js
+++ src/components/NominatedList.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import MovieCard from './MovieCard';
 
-const NominatedList = () => {
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const NominatedList = ({ nominations }) => {
   return (
-    <div></div>
+    <section>
+      <h2>Nominated Movies</h2>
+      <StyledDiv>
+        {
+          nominations.map((movie) => <MovieCard key={movie.imdbID} movie={movie}>Test</MovieCard>)
+        }
+      </StyledDiv>
+    </section>
   );
 }
  
